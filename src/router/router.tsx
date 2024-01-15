d9190299ecc6d1cf4b924cfbc2305c60876a8fad
HamsterStudent <hwanyamyu@gmail.com>
2024-01-15 18:56:08 +0900

    :sparkles: Feat : add router

---
+++ src/router/router.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Reservation from "../pages/reservation/reservation";
+import Main from "../pages/main/main";
+import Check from "../pages/check/check";
+import BusInfo from "../pages/busInfo/busInfo";
+import Announcement from "../pages/announcement/announcement";
+
+export default function Router() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/reservation" element={<Reservation />} />
+        <Route path="/check" element={<Check />} />
+        <Route path="/businfo" element={<BusInfo />} />
+        <Route path="/announcement" element={<Announcement />} />
+      </Routes>
+    </>
+  );
+}
