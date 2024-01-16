7cf908fef2cc5809f343bbc9328ac58f97610d77
HamsterStudent <hwanyamyu@gmail.com>
2024-01-16 18:04:32 +0900

    :recycle: Refact : seperate component

--- src/pages/busInfo/busInfo.tsx
+++ src/pages/busInfo/busInfo.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SubTitle from "../../shared/components/subTitle";
 import { BusTable } from "../../shared/style/tableStyle";
 import { PaddingAround, PageList } from "../../shared/style/componentStyle";
 import styled from "styled-components";
+import BusCompanyInfo from "./components/busCompanyInfo";
+import TerminalInfo from "./components/terminalInfo";
 
 const TabMenu = styled.div`
   display: flex;
@@ -13,7 +15,11 @@ const TabMenu = styled.div`
     padding: 15px 23px;
     border-radius: 22px;
     font: 700 2rem/1.5rem "Inter";
-    word-break: keep-all;
+    /* word-break: keep-all; */
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    word-break: break-all;
     margin-right: 10px;
     &:last-child {
       margin-right: none;
@@ -25,6 +31,16 @@ const TabMenu = styled.div`
 `;
 
 export default function BusInfo() {
+  const [active, setActive] = useState("버스회사 안내");
+  const tabInfo = ["버스회사 안내", "터미널 안내"];
+
+  const onClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    const {
+      currentTarget: { textContent },
+    } = e;
+    setActive(`${textContent}`);
+  };
+
   return (
     <div>
       <SubTitle>
@@ -36,37 +52,21 @@ export default function BusInfo() {
       </SubTitle>
       <PaddingAround>
         <TabMenu>
-          <div className="active">버스회사 안내</div>
-          <div>터미널 안내</div>
+          {tabInfo.map((x) => {
+            return (
+              <div
+                key={x}
+                onClick={(e) => {
+                  onClick(e);
+                }}
+                className={active === x ? `active` : undefined}
+              >
+                {x}
+              </div>
+            );
+          })}
         </TabMenu>
-        <BusTable>
-          <thead>
-            <tr className="cate">
-              <th>번호</th>
-              <th>회사명</th>
-              <th>전화번호</th>
-              <th>주소</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>경기고속</td>
-              <td>02{")"}464-6111</td>
-              <td>서울특별시 성동구 왕십리로 125, 17층</td>
-            </tr>
-          </tbody>
-        </BusTable>
-        <PageList>
-          <li className="numActive">1</li>
-          <li>2</li>
-          <li>3</li>
-          <li>4</li>
-          <li>5</li>
-          <li>6</li>
-          <li>7</li>
-          <li>8</li>
-        </PageList>
+        {active === "버스회사 안내" ? <BusCompanyInfo /> : <TerminalInfo />}
       </PaddingAround>
     </div>
   );
---
+++ src/pages/busInfo/components/busCompanyInfo.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { BusTable } from "../../../shared/style/tableStyle";
+import { PageList } from "../../../shared/style/componentStyle";
+import styled from "styled-components";
+
+const FilterResult = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 12px;
+`;
+export default function BusCompanyInfo() {
+  return (
+    <>
+      <FilterResult>
+        <select name="" id="">
+          <option value="">전체</option>
+        </select>
+        <input type="text" placeholder="회사명을 입력해 주세요" />
+      </FilterResult>
+      <BusTable>
+        <thead>
+          <tr className="cate">
+            <th>번호</th>
+            <th>회사명</th>
+            <th>전화번호</th>
+            <th>주소</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>경기고속</td>
+            <td>02{")"}464-6111</td>
+            <td>서울특별시 성동구 왕십리로 125, 17층</td>
+          </tr>
+        </tbody>
+      </BusTable>
+      <PageList>
+        <li className="numActive">1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+      </PageList>
+    </>
+  );
+}
