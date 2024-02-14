1fba9dc4c45bcdea4b59a702bdfc3c2dae85aaf4
HamsterStudent <hwanyamyu@gmail.com>
2024-02-14 13:08:30 +0900

    :lipstick: Style : update header style

--- src/shared/components/header.tsx
+++ src/shared/components/header.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { Mobile, PC } from "./responsive";
+import { Layout } from "../style/componentStyle";
 
 const HeaderWrap = styled.header`
-  margin: 74px 0 98px 0;
+  /* margin: 74px 0 98px 0; */
+  position: absolute;
+  top: 74px;
   display: grid;
+  align-content: center;
   grid-template-columns: 150px 3fr 1fr;
   word-break: keep-all;
   select {
@@ -17,11 +21,21 @@ const HeaderWrap = styled.header`
 `;
 
 const Logo = styled.div`
-  font: 400 2.1rem "Wanted Sans";
+  height: 24px;
+  a {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
 `;
 
 const List = styled.div`
-  font: 400 1.6rem "Wanted Sans";
+  font: 500 1.6rem "Wanted Sans";
   a {
     padding-left: 54px;
   }
@@ -39,26 +53,30 @@ const List = styled.div`
 
 export default function Header() {
   return (
-    <HeaderWrap>
-      <Logo>
-        <Link to={"/"}>BUSTAGO</Link>
-      </Logo>
-      <PC>
-        <List>
-          <Link to={"/reservation"}>승차권 예매</Link>
-          <Link to={"/check"}>조회/변경/취소</Link>
-          <Link to={"/businfo"}>운행정보</Link>
-          <Link to={"/announcement"}>고객지원</Link>
-        </List>
-        <List className="submenu">
-          <Link to={"/"}>로그인</Link>
-          <Link to={"/"}>회원가입</Link>
-          <Link to={"/"}>마이페이지</Link>
-          <select name="" id="">
-            <option value="">KOR</option>
-          </select>
-        </List>
-      </PC>
-    </HeaderWrap>
+    <Layout>
+      <HeaderWrap>
+        <Logo>
+          <Link to={"/"}>
+            <img src="assets/logo/logo.png" />
+          </Link>
+        </Logo>
+        <PC>
+          <List>
+            <Link to={"/reservation"}>승차권 예매</Link>
+            <Link to={"/check"}>조회/변경/취소</Link>
+            <Link to={"/businfo"}>운행정보</Link>
+            <Link to={"/announcement"}>고객지원</Link>
+          </List>
+          <List className="submenu">
+            <Link to={"/"}>로그인</Link>
+            <Link to={"/"}>회원가입</Link>
+            <Link to={"/"}>마이페이지</Link>
+            <select name="" id="">
+              <option value="">KOR</option>
+            </select>
+          </List>
+        </PC>
+      </HeaderWrap>
+    </Layout>
   );
 }
