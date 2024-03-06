5bc30185406f6ae5f7680134eff3ba70c122a016
HamsterStudent <hwanyamyu@gmail.com>
2024-03-06 11:13:37 +0900

    :lipstick: Style : reservation style edit

--- src/pages/reservation/reservation.tsx
+++ src/pages/reservation/reservation.tsx
@@ -10,6 +10,9 @@ import { BusTable } from "../../shared/style/tableStyle";
 import Notice from "../../shared/components/notice";
 import { useDialog } from "../../shared/hooks/useDialog";
 import SelectSeat from "./components/SelectSeat";
+import { useState } from "react";
+import ReservationDetail from "./components/reservationDetail";
+import { Step } from "./style/reservationStyle";
 
 const Wrapper = styled.section`
   width: 100%;
@@ -22,32 +25,17 @@ const Wrapper = styled.section`
   }
 `;
 
-const Step = styled.ul`
-  display: flex;
-  justify-content: center;
-  height: 40px;
-  margin-bottom: 30px;
-  font: 700 1.8rem "Wanted Sans";
-  li {
-    padding: 0 27px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    svg {
-      margin-right: 10px;
-    }
-  }
-  .stepActive {
-    border-radius: 22px;
-    background-color: #d9d9d9;
-  }
-`;
-
 const InfoTabMenu = styled(TabMenu)`
   width: 30%;
-  height: 40px;
+  height: 49px;
   .tab {
     font: 600 1.8rem "Wanted Sans";
+    background: url("assets/icon/tabbg-mini.png") no-repeat;
+    background-size: contain;
+  }
+  .tabActive {
+    background: url("assets/icon/tabbg-mini-active.png") no-repeat;
+    background-size: contain;
   }
 `;
 
@@ -55,22 +43,29 @@ const ReservationTabMenu = styled(TabMenu)`
   display: flex;
   justify-content: space-between;
   .tab {
-    width: 49.8%;
+    width: 50%;
     padding: 16px 26px;
     font: 700 1.8rem "Wanted Sans";
     display: flex;
     justify-content: space-between;
+    background: url("assets/icon/tabbg-reservation.png") no-repeat;
+    background-size: contain;
+  }
+  .tabActive {
+    background: url("assets/icon/tabbg-reservation-active.png") no-repeat;
+    background-size: contain;
   }
 `;
 const InfoSelect = styled.div`
   display: flex;
   justify-content: space-between;
-  border: solid 2px #d9d9d9;
+  border: solid 2px #ffe453;
   padding: 17px 33px 17px 33px;
   margin-bottom: 95px;
   h3 {
     font: 600 1.8rem "Wanted Sans";
     margin-bottom: 8px;
+    text-align: center;
   }
   .passengers {
     width: 30%;
@@ -79,6 +74,19 @@ const InfoSelect = styled.div`
     justify-content: space-between;
     padding-left: 28px;
     border-left: solid 2px #d9d9d9;
+    .number {
+      display: flex;
+      align-items: center;
+      padding: 7px;
+      width: 100%;
+      height: 40px;
+      border-radius: 20px;
+      border: solid 1px #e5e5e5;
+      font: 500 1.6rem "Wanted Sans";
+      span {
+        padding: 0 5px;
+      }
+    }
     button {
       margin-top: 24px;
       color: #fff;
@@ -87,7 +95,19 @@ const InfoSelect = styled.div`
       height: 58px;
       width: 100%;
       border-radius: 28px;
-      background: #000;
+      background: #ff8038;
+    }
+  }
+  .date {
+    width: 35%;
+    input {
+      width: 100%;
+    }
+  }
+  .grade {
+    width: 35%;
+    select {
+      width: 100%;
     }
   }
   ul {
@@ -95,6 +115,10 @@ const InfoSelect = styled.div`
     li {
       display: flex;
       justify-content: space-between;
+      margin-bottom: 30px;
+      &:last-child {
+        margin-bottom: 0;
+      }
       input {
         padding: 13px;
         height: 40px;
@@ -110,8 +134,8 @@ const Result = styled.section`
 `;
 
 export default function Reservation() {
-  const { SelectSeatDialog, openDialog, closeDialog } = useDialog();
-
+  const { SelectSeatDialog, openDialog } = useDialog();
+  const [step, setStep] = useState<"좌석선택" | "예매내역 확인">("좌석선택");
   return (
     <Layout>
       <Wrapper>
@@ -135,7 +159,7 @@ export default function Reservation() {
                 fillRule="evenodd"
                 clipRule="evenodd"
                 d="M8 22.5C8 22.5 16 15.2135 16 7.7669C16 3.47736 12.4183 0 8 0C3.58172 0 0 3.47736 0 7.7669C0 15.2135 8 22.5 8 22.5ZM8 12C10.2091 12 12 10.2091 12 8C12 5.79086 10.2091 4 8 4C5.79086 4 4 5.79086 4 8C4 10.2091 5.79086 12 8 12Z"
-                fill="#AAAAAA"
+                fill="#ff8038"
               />
             </svg>
             예매정보
@@ -176,8 +200,14 @@ export default function Reservation() {
           </li>
         </Step>
         <InfoTabMenu>
-          <div className="tab tabActive">편도</div>
-          <div className="tab">왕복</div>
+          <div className="tab tabActive">
+            <img src="assets/icon/round-trip-orange.png" alt="" />
+            왕복
+          </div>
+          <div className="tab">
+            <img src="assets/icon/one-way.png" alt="" />
+            편도
+          </div>
         </InfoTabMenu>
         <InfoSelect>
           <ul>
@@ -186,14 +216,27 @@ export default function Reservation() {
                 <h3>출발지</h3>
                 <input type="text" value={"동서울"} />
               </div>
-              <div>
+              <div className="date">
                 <h3>가는일시</h3>
                 <input type="text" value={"2024.01.01"} />
+              </div>
+              <div className="grade">
+                <h3>버스등급</h3>
                 <select name="" id="">
-                  <option value="">13:00 이후</option>
+                  <option value="">프리미엄심야우등(주말)</option>
                 </select>
               </div>
+            </li>
+            <li>
               <div>
+                <h3>도착지</h3>
+                <input type="text" value={"동서울"} />
+              </div>
+              <div className="date">
+                <h3>오는일시</h3>
+                <input type="text" value={"2024.01.01"} />
+              </div>
+              <div className="grade">
                 <h3>버스등급</h3>
                 <select name="" id="">
                   <option value="">프리미엄심야우등(주말)</option>
@@ -204,21 +247,27 @@ export default function Reservation() {
           <div className="passengers">
             <div>
               <h3>성인</h3>
-              <select name="" id="">
-                <option value="">10명</option>
-              </select>
+              <div className="number">
+                <img src="assets/icon/minus.png" alt="" />
+                <span>10명</span>
+                <img src="assets/icon/plus.png" alt="" />
+              </div>
             </div>
             <div>
               <h3>중고생</h3>
-              <select name="" id="">
-                <option value="">10명</option>
-              </select>
+              <div className="number">
+                <img src="assets/icon/minus.png" alt="" />
+                <span>10명</span>
+                <img src="assets/icon/plus.png" alt="" />
+              </div>
             </div>
             <div>
               <h3>아동</h3>
-              <select name="" id="">
-                <option value="">10명</option>
-              </select>
+              <div className="number">
+                <img src="assets/icon/minus.png" alt="" />
+                <span>10명</span>
+                <img src="assets/icon/plus.png" alt="" />
+              </div>
             </div>
             <button>조회하기</button>
           </div>
@@ -230,11 +279,36 @@ export default function Reservation() {
         <Result>
           <ReservationTabMenu>
             <div className="tab tabActive">
-              <div className="direction">동서울 ... 동대구</div>
-              <div className="date">2024.01.01</div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="23"
+                viewBox="0 0 16 23"
+                fill="none"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M8 22.5C8 22.5 16 15.2135 16 7.7669C16 3.47736 12.4183 0 8 0C3.58172 0 0 3.47736 0 7.7669C0 15.2135 8 22.5 8 22.5ZM8 12C10.2091 12 12 10.2091 12 8C12 5.79086 10.2091 4 8 4C5.79086 4 4 5.79086 4 8C4 10.2091 5.79086 12 8 12Z"
+                  fill="#ff8038"
+                />
+              </svg>
+              <div className="direction">
+                동서울
+                <img src="assets/icon/one-way-dotted-active.png" alt="" />
+                동대구
+              </div>
+              <div className="date">
+                <img src="assets/icon/calendar.png" alt="" />
+                2024.01.01
+              </div>
             </div>
             <div className="tab">
-              <div className="direction">동대구 ... 동서울</div>
+              <div className="direction">
+                동대구
+                <img src="assets/icon/one-way-dotted.png" alt="" />
+                동서울
+              </div>
               <div className="date">2024.01.01</div>
             </div>
           </ReservationTabMenu>
@@ -260,17 +334,22 @@ export default function Reservation() {
                 <td>소노캄호텔(엠블호텔)</td>
                 <td>대전청사공원(선사유적)K</td>
                 <td>대원고속</td>
-                <td>프리미엄심야우등(주말)</td>
+                <td>
+                  프리미엄심야우등(주말)<span className="type">직통</span>
+                </td>
                 <td>30</td>
                 <td>30</td>
                 <td>X</td>
-                <td>
+                <td className="grey">
                   <button>상세보기</button>
                 </td>
-                <td>
+                <td className="orange">
                   <button onClick={openDialog}>좌석선택</button>
                   <SelectSeatDialog title="좌석선택">
-                    <SelectSeat />
+                    {step === "좌석선택" && (
+                      <SelectSeat onNext={() => setStep("예매내역 확인")} />
+                    )}
+                    {step === "예매내역 확인" && <ReservationDetail />}
                   </SelectSeatDialog>
                 </td>
               </tr>
